28b4f074dd48d7af50a990343c4ed2229623ad96
liujin <2454243839@qq.com>
2023-02-24 16:56:44 +0800

    更新

--- src/international/config.js
+++ src/international/config.js
@@ -2,8 +2,8 @@ const shineUpon = require('./locales/en-US.json')
 const externalFiles = require('./external/externalFiles')
 const globalData = {
   shineUpon: shineUpon,//映射文件 加入则不会重复翻译映射文件中存在的值
-  appid: '20230213001560478',
-  key: 'hBrwAmNN0bp22OiSmNV_',
+  appid: '',//百度翻译的appid
+  key: '',//百度翻译的key  申请网址 https://fanyi-api.baidu.com/manage/developer
   delay: 1200,
   to: 'th',//中zh,俄ru,日jp,英en,荷兰nl,泰国th
   isExternalFiles: true,//是否为外部翻译文件 针对所罗门项目
